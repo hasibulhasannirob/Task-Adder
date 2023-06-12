d318fec5fb1674ba395c31d5d343a0c74021f6bd
hasibulhasannirob <hasibulnirob987@gmail.com>
2023-06-12 22:29:26 +0600

    Frontend and add task

---
+++ App.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
+
+export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHeader(enteredtext) {
+    setEnteredGoalText(enteredtext);
+  }
+  function addGoalHandle() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Goal"
+          onChangeText={goalInputHeader}
+        />
+        <Button color="#7E37F3" title="ADD GOAL" onPress={addGoalHandle} />
+      </View>
+      <View style={styles.goalContainer}>
+        <ScrollView>
+          {courseGoals.map((goal) => (
+            <View key={goal} style={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#2BEBDD",
+  },
+  goalText: {
+    color: "black",
+    
+  },
+});
---
+++ components/goalItem.js
@@ -0,0 +1,10 @@
+function goalItem() {
+
+    return(
+        <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{goal}</Text>
+      </View>
+    );
+}
+export default goalItem;
+const styles = StyleSheet.create({});
